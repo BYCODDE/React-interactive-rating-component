a1cfc046287570e2c3fa3cae4a3c5706f3c694a1
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-16 18:33:49 +0400

    new commit!

--- src/components/Ovals/Ovals.jsx
+++ src/components/Ovals/Ovals.jsx
@@ -1,15 +1,32 @@
-import "./oval.css"
-import Oval from "/images/Oval.svg"
+import "./oval.css";
+import Oval from "/images/Oval.svg";
 function Ovals() {
   return (
     <section className="oval_section">
-        <img src={Oval} alt=""  />
-        <img src={Oval} alt="" />
-        <img src={Oval} alt="" />
-        <img src={Oval} alt="" />
-        <img src={Oval} alt="" />
+      <div>
+        <img src={Oval} alt="Oval1" />
+        <p className="img_p">1</p>
+      </div>
+
+      <div>
+        <img src={Oval} alt="Oval2" />
+        <p className="img_p">2</p>
+      </div>
+
+      <div>
+        <img src={Oval} alt="Oval3" />
+        <p className="img_p">3</p>
+      </div>
+      <div>
+        <img src={Oval} alt="Oval4" />
+        <p className="img_p">4</p>
+      </div>
+      <div>
+        <img src={Oval} alt="Oval5" />
+        <p className="img_p">5</p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Ovals
+export default Ovals;
