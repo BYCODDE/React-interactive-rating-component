0f313b1e14a035fbe2b7890e2e448213bc8ac2b4
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-17 14:32:30 +0400

    new commit!

--- src/components/Ovals/Ovals.jsx
+++ src/components/Ovals/Ovals.jsx
@@ -14,30 +14,22 @@ function Ovals({ submit, handleClick, buttonClicked }) {
       <div onClick={() => handleClick(2)}>
         <img src={Oval} alt="Oval2" />
         <p className="img_p">2</p>
-        {submit === 2 && buttonClicked ? (
-          <p>You selected {submit} out of 5</p>
-        ) : null}
+        {submit === 2 && buttonClicked ? <Main2 submit={submit}></Main2> : null}
       </div>
       <div onClick={() => handleClick(3)}>
         <img src={Oval} alt="Oval3" />
         <p className="img_p">3</p>
-        {submit === 3 && buttonClicked ? (
-          <p>You selected {submit} out of 5</p>
-        ) : null}
+        {submit === 3 && buttonClicked ? <Main2 submit={submit}></Main2> : null}
       </div>
       <div onClick={() => handleClick(4)}>
         <img src={Oval} alt="Oval4" />
         <p className="img_p">4</p>
-        {submit === 4 && buttonClicked ? (
-          <p>You selected {submit} out of 5</p>
-        ) : null}
+        {submit === 4 && buttonClicked ? <Main2 submit={submit}></Main2> : null}
       </div>
       <div onClick={() => handleClick(5)}>
         <img src={Oval} alt="Oval5" />
         <p className="img_p">5</p>
-        {submit === 5 && buttonClicked ? (
-          <p>You selected {submit} out of 5</p>
-        ) : null}
+        {submit === 5 && buttonClicked ? <Main2 submit={submit}></Main2> : null}
       </div>
     </section>
   );
