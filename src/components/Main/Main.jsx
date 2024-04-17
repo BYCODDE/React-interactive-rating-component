14aba57963d020507e378d31fe08e2c3615921d3
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-17 15:08:43 +0400

    new commit!

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,8 @@
 import "./main.css";
-
 import Star from "/images/Star.svg";
 import Text from "../Text/Text";
 import Ovals from "../Ovals/Ovals";
 import Button from "../Button/Button";
-
 import { useState } from "react";
 function Main() {
   const [submit, setSubmit] = useState(null);
