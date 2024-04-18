ff04a8ac1d372cad6ef5ca82854618eb3d9bf535
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-18 11:07:53 +0400

    final commit!

--- src/components/Ovals/Ovals.jsx
+++ src/components/Ovals/Ovals.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Main2 from "../../components2/Main/Main2";
 import "./oval.css";
-import Oval from "/images/Oval.svg";
 
 function Ovals({ submit, handleClick, buttonClicked }) {
   return (
@@ -10,6 +10,7 @@ function Ovals({ submit, handleClick, buttonClicked }) {
         <p className="img_p">1</p>
         {submit === 1 && buttonClicked ? <Main2 submit={submit}></Main2> : null}
       </div>
+
       <div className="oval_div" onClick={() => handleClick(2)}>
         <p className="img_p">2</p>
         {submit === 2 && buttonClicked ? <Main2 submit={submit}></Main2> : null}
